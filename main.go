0aae00f17780ad9ea36d2a6e1bb892393cd8c017
Gaurav Gosain <itsgauravgosain@gmail.com>
2024-05-14 22:32:51 +0400

    init

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	// fires only for the "dubbing" collection
+	app.OnRecordAfterCreateRequest("dubbing").Add(func(e *core.RecordCreateEvent) error {
+		originalVideos := e.UploadedFiles["original_video"]
+
+		if len(originalVideos) == 0 {
+			return errors.New("video not uploaded")
+		}
+
+		originalVideo := originalVideos[0]
+
+		originalVideoUrl := fmt.Sprintf("pb_data/storage/%s/%s/%s", e.Collection.Id, e.Record.Id, originalVideo.Name)
+
+		if _, err := os.Stat(originalVideoUrl); err != nil {
+			return err
+		}
+
+		outputPath := originalVideoUrl + ".mp4"
+		outputFileName := originalVideo.Name + ".mp4"
+
+		err := ffmpeg.Input(originalVideoUrl).
+			Output(
+				outputPath,
+				// ffmpeg.KwArgs{"c:v": "libx264"},
+				// ffmpeg.KwArgs{"c:a": "aac"},
+				// ffmpeg.KwArgs{"strict": "experimental"},
+			).
+			OverWriteOutput().ErrorToStdOut().Run()
+		if err != nil {
+			return err
+		}
+
+		record := e.Record
+		record.Set("original_video", outputFileName)
+
+		err = app.Dao().SaveRecord(record)
+		if err != nil {
+			return err
+		}
+
+		// delete the originalVideoUrl
+		err = os.Remove(originalVideoUrl)
+		if err != nil {
+			return nil
+		}
+
+		return nil
+	})
+
+	app.OnRecordAfterDeleteRequest("dubbing").Add(func(e *core.RecordDeleteEvent) error {
+		recordData := fmt.Sprintf("pb_data/storage/%s/%s", e.Collection.Id, e.Record.Id)
+
+		err := os.RemoveAll(recordData)
+		return err
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
